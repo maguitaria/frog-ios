8247ea3ec71de80565fd1804d9d4b0c085a646b0
Mariia Glushenkova <mglushen22@students.oamk.fi>
2025-03-28 18:03:07 +0100

    Initial Commit

---
+++ ios-angry-fog/ios_angry_fogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_angry_fogApp.swift
+//  ios-angry-fog
+//
+//  Created by Mariia Glushenkova on 28.3.2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_angry_fogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
